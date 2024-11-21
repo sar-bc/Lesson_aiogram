e4aeabe14722e7469597a166ea7ed066c9f4ee29
Виктор <sar-bc@mail.ru>
2024-11-21 04:08:09 +0400

    Lesson_2_router

---
+++ app/handlers.py
@@ -0,0 +1,18 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, CommandObject
+
+router = Router()
+
+@router.message(CommandStart(deep_link=True, magic=F.args.isdigit()))
+async def cmd_start(message: Message, command: CommandObject):
+    await message.answer(f'Привет! Ты пришел от {command.args}')
+
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фотографии: {message.photo[-1].file_id}')
+
+@router.message(F.text == '123')
+async def echo(message: Message):
+    await message.answer("hello")    
--- main.py
+++ main.py
@@ -1,36 +1,24 @@
+import asyncio
+import logging
 import os
 from dotenv import load_dotenv
-import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import CommandStart
-import logging
+
+
+from app.handlers import router
 
 load_dotenv()
 
-
-
-bot = Bot(token=os.getenv("TOKEN"))
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer("Привет")
-
-@dp.message()
-async def echo(message: Message):
-    await message.answer("Неизвестная команда")
-
 async def main():
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
-
-if __name__== '__main__':
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
-    except KeyboardInterrupt:
-        print('Бот выключен')
-
+    except:
+        print('Exit')
